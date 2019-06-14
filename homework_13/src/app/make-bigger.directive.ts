8a506c0bd0cae66bcc71e984a22743f064f858a4
Gebreslassiekah <44591226+Gebreslassiekah@users.noreply.github.com>
2019-06-13 23:27:56 -0500

    update

--- homework_13/src/app/make-bigger.directive.ts
+++ homework_13/src/app/make-bigger.directive.ts
@@ -4,10 +4,12 @@ import { Directive ,ElementRef,Renderer,HostListener} from '@angular/core';
   selector: '[appMakeBigger]'
 })
 export class MakeBiggerDirective {
-
+private size=14;
   constructor(private element: ElementRef, private renderer: Renderer) { }
-  @HostListener('dblclick') dblClick() {
-    this.element.nativeElement.style.forntSize='555px';
+  @HostListener('dblclick') 
+  onDoubleClick() {
+    this.size= this.size+2;
+    this.element.nativeElement.style.fontSize=`${this.size}px`;
   console.log('bigger element double-clicked');
 }
 }
